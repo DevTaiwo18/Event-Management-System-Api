d0425a66210ccff001aa6c3403a2ba4da3d0054e
DevTaiwo <adeyemitaiwo24434@gmail.com>
2024-03-12 18:47:52 -0700

    all files added

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,5 @@
 const handleCastErrorDB = (err) => {
-    const message = `Invalid ${err.path}: ${err.value}.`;
+    const message = `${err.value}.`;
     const error = new Error(message);
     error.statusCode = 400;
     return error
